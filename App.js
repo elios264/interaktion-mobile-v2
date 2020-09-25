d5e3a253dc36a9f8f42b6e1c83237a2dde20386f
Luis Alberto <alberto@altomobile.io>
2020-09-25 14:12:46 -0500

    + ui library

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Button, IconRegistry, Icon } from '@ui-kitten/components';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+
+import { default as theme } from './custom-theme.json';
+import { default as mapping } from './mapping.json';
+
+const FacebookIcon = (props) => (
+  <Icon name='facebook' {...props} />
+);
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'osl': require('./assets/fonts/Oswald.ttf'),
+    'osl_b': require('./assets/fonts/Oswald_bold.ttf'),
+    'osl_bi': require('./assets/fonts/Oswald_bold_italic.ttf'),
+    'al': require('./assets/fonts/AbrahamLincoln.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} customMapping={mapping} >
+        <StatusBar style='inverted' />
+        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+          <Text category='h1'>HOME</Text>
+          <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+        </Layout>
+      </ApplicationProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # interaktion-mobile-v2
 Mobile client with Expo
+
+- setup ui
+
+- Setup project
+- Setup eslint
+- setup navigation
+- setup splash and icons
+- setup status bar
+- setup ota
+- setup env variables
+- setup userInterfaceStyle, backgroundColor, primaryColor, entryPoint (registerRootComponent), supportsTablet
+- use AppLoading
+- preload assets, font
+- connect storage to parse SecureStore
+- calendar for events
+- constants for Constants installations push
+- expo-localization
+- shared element
+- push notifications
+- SafeAreaContext
+- lock orientation
+- ViewPager between contents
+- webBrowser for external links
